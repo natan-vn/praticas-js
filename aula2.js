907901339ab9151fd6e64f69605c785b86eecc7b
natan-vn <natanaelboa123@gmail.com>
2025-01-31 21:43:11 -0300

    Meu primeiro commit

---
+++ aula2.js
@@ -0,0 +1,7 @@
+// TIPOS DE DADOS: PRIMITIVOS
+
+let string =  "Bastião"
+let number = "33"
+let booleano = true
+let valorPadrao = undefined
+let vazioOpcional = null
---
+++ aula3.js
@@ -0,0 +1,13 @@
+// OBJETOS
+
+let pessoa = {
+    nome: 'Paulo',
+    idade: '99',
+    profissao: 'FrontEnd'
+}
+
+console.log(pessoa)
+console.log(typeof pessoa)
+
+console.log(pessoa.nome)
+console.log(typeof pessoa.idade)
